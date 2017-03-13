670a51c0e0b849650e4b70066c344bf9474f4167
Tim <Tim@DESKTOP-A0FHQBU>
2017-03-13 17:07:54 -0500

    Added all of the models and changed the home page.

---
+++ src/CureSort/Models/MedicalDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CureSort.Models
+{
+    public class MedicalDevice
+    {
+    }
+}
